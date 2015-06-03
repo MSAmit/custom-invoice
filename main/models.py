174835b0f43d67186d3409254f8dde263b52fd63
Amit M S <shanksamit123@gmail.com>
2015-06-03 12:25:35 +0530

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class NPN(models.Model):
+	name = models.CharField(max_length=150)
+	quantity = models.CharField(max_length=150)
+	price = models.CharField(max_length=100)
 
+	def __unicode__(self):
+		return self.name
+class Lube(models.Model):
+	name = models.CharField(max_length=150)
+	quantity = models.CharField(max_length=150)
+	price = models.CharField(max_length=100)
 
+	def __unicode__(self):
+		return self.name
 class Spare(models.Model):
 	name = models.CharField(max_length=150)
+	quantity = models.CharField(max_length=150)
 	price = models.CharField(max_length=100)
 
 	def __unicode__(self):
